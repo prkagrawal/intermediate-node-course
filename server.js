60f4e2a506db821f8f5e9d733df55270437dcd41
Prince Agrawal <princeagrawalnew123@gmail.com>
2020-08-28 10:48:30 +0530

    refactored code

--- server.js
+++ server.js
@@ -16,104 +16,79 @@ app.listen(port, ()=>{
 	console.log(`server is listening on port:${port}`)
 })
 
+function sendResponse(res,err,data){
+  if (err){
+    res.json({
+      success: false,
+      message: err
+    })
+  } else if (!data){
+    res.json({
+      success: false,
+      message: "Not Found"
+    })
+  } else {
+    res.json({
+      success: true,
+      data: data
+    })
+  }
+}
+
+// We can shorten these routes further by using the JavaScript spread syntax, which copies each key/value pair in our newData object.
+
+// So, rather than this:
+
+// {
+//   name:req.body.newData.name,
+//   email:req.body.newData.email,
+//   password:req.body.newData.password
+// }
+// we can write this:{...req.body.newData}.
+
 // CREATE
 app.post('/users',(req,res)=>{
-  User.create(
-	  {
-	  	name:req.body.newData.name,
-	  	email:req.body.newData.email,
-	  	password:req.body.newData.password
-	  },
-	  (err,data) => {
-	  	if (err) {
-	  		res.json({ success: false, message: err })
-	  	} else if (!data) {
-	  		res.json({ success: false, message: "Not Found" })
-	  	} else {
-	  		res.json({ success: true, data: data })
-	  	}
-	  }
-  )
+	User.create(
+		{
+			...req.body.newData
+		},
+		(err,data) => {
+			sendResponse(res,err,data)
+		}
+	)
 })
 
 app.route('/users/:id')
 // READ
 .get((req,res)=>{
-  // User.findById()
-  User.findById(req.params.id,(err,data)=>{
-    if (err){
-      res.json({
-        success: false,
-        message: err
-      })
-    } else if (!data){
-      res.json({
-        success: false,
-        message: "Not Found"
-      })
-    } else {
-      res.json({
-        success: true,
-        data: data
-      })
-    }
-  })
+	// User.findById()
+	User.findById(req.params.id,(err,data)=>{
+		sendResponse(res,err,data)
+	})
 })
 // UPDATE
 .put((req,res)=>{
-  // User.findByIdAndUpdate()
-  User.findByIdAndUpdate(
-    req.params.id,
-    {
-      name:req.body.newData.name,
-      email:req.body.newData.email,
-      password:req.body.newData.password
-    },
-    {
-      new:true
-    },
-    (err,data)=>{
-      if (err){
-        res.json({
-          success: false,
-          message: err
-        })
-      } else if (!data){
-        res.json({
-          success: false,
-          message: "Not Found"
-        })
-      } else {
-        res.json({
-          success: true,
-          data: data
-        })
-      }
-    }
-  )
+	// User.findByIdAndUpdate()
+	User.findByIdAndUpdate(
+		req.params.id,
+		{
+			...req.body.newData
+		},
+		{
+			new:true
+		},
+		(err,data)=>{
+			sendResponse(res,err,data)
+		}
+	)
 })
 // DELETE
 .delete((req,res)=>{
-  // User.findByIdAndDelete()
-  User.findByIdAndDelete(
-    req.params.id,
-    (err,data)=>{
-      if (err){
-        res.json({
-          success: false,
-          message: err
-        })
-      } else if (!data){
-        res.json({
-          success: false,
-          message: "Not Found"
-        })
-      } else {
-        res.json({
-          success: true,
-          data: data
-        })
-      }
-    }
-  )
+	// User.findByIdAndDelete()
+	User.findByIdAndDelete(
+		req.params.id,
+		(err,data)=>{
+			sendResponse(res,err,data)
+		}
+	)
 })
